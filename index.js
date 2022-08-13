1d41ae71e194d3134a39f4585914ccb9e9b77949
hamsa <hafieekhan@gmail.com>
2022-08-13 16:26:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Hamsa",
+    streetAddress: "11 Broadway",
+}
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+    newObj[key] = value;
+
+  return newObj;
+    // Code to return new, updated menu object goes here
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value; //Why are we using bracket notation here?
+  
+    return obj;
+  }
+  function deleteFromEmployeeByKey(obj, key, value) {
+    const newObj = { ...obj };
+    newObj[key] = value;
+
+  return newObj;
+    // Code to return new, updated menu object goes here
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
+    obj[key] = value; //Why are we using bracket notation here?
+  
+    return obj;
+  }
